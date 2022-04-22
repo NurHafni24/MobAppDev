be57a3a716dbce5a1418acb6baedba82980a4efa
FarjanaMammmi <75902424+FarjanaMammmi@users.noreply.github.com>
2022-04-23 06:26:45 +0800

    Update PointOfSale.dart

--- PointOfSale.dart
+++ PointOfSale.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
+// this variables should never be global. (too lazy to fix that as well )
 double newPrice = 0;
 double disc = 0; //Discount price
 double totalAfterDiscount = 0;
+double gstValue = 0.0;
 
 class Item {
   //late keyword is to tell the compiler to treat this variable as non-nullable and will be initialized later
@@ -22,7 +24,8 @@ class Item {
 void main() {
   List<Item> itemList = []; //To store products inserted
 
-  while (true) { //Let user choose
+  while (true) {
+    //Let user choose
     print('\n');
 
     print("Point Of Sale System: \n");
@@ -33,93 +36,110 @@ void main() {
     print("2. List of products"); //print product list with the detail
 
     print("3. Total amount after discount");
+    print("4. Set GST percent (default = 0.6% )");
 
-    print("4. Make Payment \n");
+    print("5. Make Payment \n");
 
     stdout.write("Enter your choice: ");
-    int input = int.parse(stdin.readLineSync()!);
-    print("\n");
 
-    if (input == 1) {
-      createItem(itemList);
-    } else if (input == 2) {
-      //code Asimah
-    } else if (input == 3){
-      afterDiscountPrice();
-    } else {
-      makePayment();
-      break;
+    // rule 1. never trust the user we implementing some error handling
+    try {
+      int input = int.parse(stdin.readLineSync()!);
+
+      if (input > 3 || input < 0) {
+        print("ERROR: Pls select a number a number between 1 - 3");
+        continue;
+      }
+      if (input == 1) {
+        createItem(itemList);
+      } else if (input == 2) {
+        //code Asimah
+      } else if (input == 3) {
+        afterDiscountPrice();
+      } else if (input == 4) {
+        gstValue = double.parse(stdin.readLineSync()!);
+      } else {
+        makePayment();
+        break;
+      }
+    } on FormatException {
+      print("ERROR: Pls an interger number");
     }
   }
 }
 
-void createItem(List<Item> items) {
+void getGST(double productPrice) {
+  gstValue == 0.0 ? gstValue = 0.6 : gstValue;
+  double totalGST = productPrice * (gstValue / (100 + gstValue));
+  // double finalAmount = totalGST + productPrice;
+  print("Total GST for 1 pack is: " + totalGST.toStringAsFixed(3));
+}
 
+void createItem(List<Item> items) {
   stdout.write("Enter product description:");
 
   print('\n');
 
   stdout.write("Name: ");
-  String input1 = stdin.readLineSync()!;
+  String productName = stdin
+      .readLineSync()!; // tf is input1, input2, input3 ? use some meaningful variable names
 
   stdout.write("Price: ");
-  double input2 = double.parse(stdin.readLineSync()!);
+  double productPrice = double.parse(stdin.readLineSync()!);
 
   stdout.write("Quantity: ");
-  double input3 = double.parse(stdin.readLineSync()!);
+  double productQuantity = double.parse(stdin.readLineSync()!);
 
-  var newItem = Item(input1, input2, input3);
+  var newItem = Item(productName, productPrice, productQuantity);
 
   items.add(newItem);
 
-  calcQuantity(newItem.price, input3, newItem.name);
+  calcQuantity(newItem.price, productQuantity, newItem.name);
 
   print("Product inserted at ${newItem.created_at}");
 }
 
 void calcQuantity(double price, double quantity, String productName) {
-
-  var payment = discount(productName, price); //Total price after calculate with quantity inserted
+  var payment = discount(
+      productName, price); //Total price after calculate with quantity inserted
 
   totalAfterDiscount = totalAfterDiscount + (payment * quantity);
-
 }
 
 //Declare discount for few items
 discount(String prodName, double prodPrice) {
-  if (prodName == "Milo" || prodName == "milo") {
+  prodName
+      .toLowerCase(); // having this line will save ur time if (x == 'AA' || x == 'aa')
 
+  // also instead of using if statement use switch. (am lazy to write that)
+  // something like this switch (productName) {case "milo": }...
+
+  if (prodName == "milo") {
     disc = prodPrice * 0.15;
     newPrice = prodPrice - disc;
     print("Price After Discount for 1 pack ${newPrice}");
-
-  } else if (prodName == "Downy" || prodName == "downy") {
-
+  } else if (prodName == "downy") {
     disc = prodPrice * 0.3;
     newPrice = prodPrice - disc;
     print("Price After Discount for 1 bottle ${newPrice}");
-
   } else if (prodName == "Chipsmore" || prodName == "chipsmore") {
-
     disc = prodPrice * 0.1;
     newPrice = prodPrice - disc;
     print("Price After Discount for 1 pack ${newPrice}");
-
   } else if (prodName == "Bowl" || prodName == "bowl") {
-
     disc = prodPrice * 0.7;
     newPrice = prodPrice - disc;
     print("Price After Discount for 1 bowl ${newPrice}");
-
   } else {
-
     newPrice = prodPrice;
-
   }
+  getGST(prodPrice);
   return newPrice;
 }
 
 afterDiscountPrice() {
+  // why not passing the value to the function instead of making it global?
+  // u remember making globals is bad programming practice??
   print("The total amount after the discount is RM ${totalAfterDiscount} \n");
 }
 
@@ -132,12 +152,12 @@ makePayment() {
   stdout.write("Enter your money: ");
   double money = double.parse(stdin.readLineSync()!);
 
-  while (money<totalAfterDiscount) {
+  while (money < totalAfterDiscount) {
     print("Not enough money T.T");
     stdout.write("Enter your money: ");
     double money = double.parse(stdin.readLineSync()!);
 
-    if (money>=totalAfterDiscount) {
+    if (money >= totalAfterDiscount) {
       double balance = money - totalAfterDiscount;
       balance.toStringAsFixed(2);
       print("TOTAL  : RM ${totalAfterDiscount}");
@@ -148,5 +168,4 @@ makePayment() {
       break;
     }
   }
-
 }
